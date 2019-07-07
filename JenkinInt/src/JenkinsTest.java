f8c668a8af5bdf28821bc1f26ed36e06f5370598
Babu <inmbabu02@ad.harman.com>
2019-07-07 14:06:40 +0530

    Committing First Method

---
+++ JenkinInt/src/JenkinsTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	WebDriver driver;
+	
+@Test
+public void browserlaunch() {
+	
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.cricbuzz.com");
+	System.out.println(driver.getTitle());
+	driver.close();
+}
+
+}
